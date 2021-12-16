60f93c65d1d3d5714a5f78f75ac9b691f53b5f36
edehlol <edymusajev@gmail.com>
2021-12-16 21:26:34 +0800

    upgraded tailwind to 3.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,11 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       fontSize: {
         xxs: '.50rem',
       },
     },
-  },
-  variants: {
-    extend: {},
   },
   plugins: [],
 };
