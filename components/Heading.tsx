86f813f295df9f245cb81434984d4bceb5b0befb
edehlol <edymusajev@gmail.com>
2022-03-08 12:10:54 +0800

    added mobile menu

--- components/Heading.tsx
+++ components/Heading.tsx
@@ -11,11 +11,18 @@ enum Align {
   CENTER,
   RIGHT,
 }
+enum Weight {
+  NORMAL,
+  MEDIUM,
+  SEMIBOLD,
+  BOLD,
+}
 
 interface Props {
   children: React.ReactNode;
   align: Align;
   size: Size;
+  weight: Weight;
 }
 
 const SIZE_MAPS: Record<Size, string> = {
@@ -29,18 +36,30 @@ const ALIGN_MAPS: Record<Align, string> = {
   [Align.CENTER]: 'text-center',
   [Align.RIGHT]: 'text-right',
 };
+const WEIGHT_MAPS: Record<Weight, string> = {
+  [Weight.NORMAL]: 'font-normal',
+  [Weight.MEDIUM]: 'font-medium',
+  [Weight.SEMIBOLD]: 'font-semibold',
+  [Weight.BOLD]: 'font-bold',
+};
 
 const Heading = (props: Props) => {
-  const { children, align, size } = props;
-  return <h2 className={classNames(SIZE_MAPS[size], ALIGN_MAPS[align])}>{children}</h2>;
+  const { children, align, size, weight } = props;
+  return (
+    <h2 className={classNames(SIZE_MAPS[size], ALIGN_MAPS[align], WEIGHT_MAPS[weight])}>
+      {children}
+    </h2>
+  );
 };
 
 Heading.defaultProps = {
   align: Align.LEFT,
   size: Size.MEDIUM,
+  weight: Weight.NORMAL,
 };
 
 Heading.size = Size;
 Heading.align = Align;
+Heading.weight = Weight;
 
 export { Heading };
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,10 @@
 import { useState } from 'react';
 import { HiMenu, HiSearch } from 'react-icons/hi';
-import { Popover } from '@headlessui/react';
+import { Popover, Transition } from '@headlessui/react';
 import { usePopper } from 'react-popper';
 import { Link } from './Link';
 import { useRouter } from 'next/dist/client/router';
+import { Heading } from './Heading';
 
 interface DropDownMenuProps {
   children: React.ReactNode;
@@ -13,6 +14,106 @@ interface DropDownMenuProps {
   }[];
 }
 
+interface PageLinks {
+  header: string;
+  links: {
+    title: string;
+    href: string;
+  }[];
+}
+
+const movieLinks: PageLinks = {
+  header: 'Movies',
+  links: [
+    {
+      title: 'Popular',
+      href: '/movie/popular',
+    },
+    {
+      title: 'Now Playing',
+      href: '/movie/playing',
+    },
+    {
+      title: 'Upcoming',
+      href: '/movie/upcoming',
+    },
+    {
+      title: 'Top Rated',
+      href: '/movie/top',
+    },
+  ],
+};
+
+const tvLinks: PageLinks = {
+  header: 'TV Shows',
+  links: [
+    {
+      title: 'Popular',
+      href: '/tv/popular',
+    },
+    {
+      title: 'Airing Today',
+      href: '/tv/airing',
+    },
+    {
+      title: 'On TV',
+      href: '/tv/on-tv',
+    },
+    {
+      title: 'Top Rated',
+      href: '/tv/top',
+    },
+  ],
+};
+const peopleLinks: PageLinks = {
+  header: 'People',
+  links: [{ title: 'Popular People', href: '/person/popular' }],
+};
+
+const MobileMenu = () => {
+  const [isShowing, setIsShowing] = useState(false);
+
+  const renderLinks = (links: PageLinks) => {
+    const linkList = links.links.map((link) => (
+      <li key={link.href} className="mb-1">
+        <Link href={link.href}>{link.title}</Link>
+      </li>
+    ));
+    return (
+      <div className="py-2">
+        <Heading size={Heading.size.MEDIUM} weight={Heading.weight.SEMIBOLD}>
+          {links.header}
+        </Heading>
+        <ul className="pt-2">{linkList}</ul>
+      </div>
+    );
+  };
+
+  return (
+    <div className="relative w-full">
+      <button onClick={() => setIsShowing(!isShowing)}>
+        <HiMenu className="text-2xl" />
+      </button>
+      <div className="fixed mt-4 z-50 w-4/5 h-full -ml-4 ">
+        <Transition
+          show={isShowing}
+          enter="transform  duration-300"
+          enterFrom="-translate-x-full"
+          enterTo="translate-x-0"
+          leave="transform duration-300"
+          leaveFrom="translate-x-0"
+          leaveTo="-translate-x-full"
+        >
+          <div className="w-4/5 h-screen bg-gray-900 px-5 py-5">
+            {renderLinks(movieLinks)}
+            {renderLinks(tvLinks)}
+            {renderLinks(peopleLinks)}
+          </div>
+        </Transition>
+      </div>
+    </div>
+  );
+};
 const DropdownMenu = (props: DropDownMenuProps) => {
   const { children, links } = props;
   let [referenceElement, setReferenceElement] = useState<HTMLButtonElement | null>();
@@ -50,48 +151,11 @@ const DropdownMenu = (props: DropDownMenuProps) => {
 };
 
 const Navigation = () => {
-  const movieLinks = [
-    {
-      title: 'Popular',
-      href: '/movie/popular',
-    },
-    {
-      title: 'Now Playing',
-      href: '/movie/playing',
-    },
-    {
-      title: 'Upcoming',
-      href: '/movie/upcoming',
-    },
-    {
-      title: 'Top Rated',
-      href: '/movie/top',
-    },
-  ];
-  const tvLinks = [
-    {
-      title: 'Popular',
-      href: '/tv/popular',
-    },
-    {
-      title: 'Airing Today',
-      href: '/tv/airing',
-    },
-    {
-      title: 'On TV',
-      href: '/tv/on-tv',
-    },
-    {
-      title: 'Top Rated',
-      href: '/tv/top',
-    },
-  ];
-  const peopleLinks = [{ title: 'Popular People', href: '/person/popular' }];
   return (
     <div className="flex gap-x-8">
-      <DropdownMenu links={movieLinks}>Movies</DropdownMenu>
-      <DropdownMenu links={tvLinks}>TV Shows</DropdownMenu>
-      <DropdownMenu links={peopleLinks}>People</DropdownMenu>
+      <DropdownMenu links={movieLinks.links}>Movies</DropdownMenu>
+      <DropdownMenu links={tvLinks.links}>TV Shows</DropdownMenu>
+      <DropdownMenu links={peopleLinks.links}>People</DropdownMenu>
     </div>
   );
 };
@@ -100,7 +164,10 @@ export const Navbar = () => {
   return (
     <div className=" flex items-center justify-center w-full bg-gray-900 text-white">
       <header className="flex items-center justify-between w-full p-4">
-        <HiMenu className="text-2xl md:hidden" />
+        <div className="md:hidden">
+          <MobileMenu />
+        </div>
+
         <div className="md:order-2 hidden md:block">
           <Navigation />
         </div>
